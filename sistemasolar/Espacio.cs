c77e42389cd2e28a666c635a94176efe8310957e
seven-pm <58634881+seven-pm@users.noreply.github.com>
2019-12-07 23:35:10 -0300

    Add files via upload

--- sistemasolar/Espacio.cs
+++ sistemasolar/Espacio.cs
@@ -1,29 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace SistemaSolar
 {
+    public enum TipoPeriodo
+    {
+        Sequia = 1,
+        Lluvia,
+        COPT,
+        Normal
+    };
+
+    /// <summary>
+    /// Representa al sistema solar.
+    /// </summary>
     public class Espacio
     {
         public IList<Planeta> Planetas { get; private set; }
         public int DiaActual { get; private set; }
         public bool Sequia { get; private set; }
         public bool Lluvia { get; private set; }
-        public bool CondicionesOptimasPyT { get; private set; }
+        public bool COPT { get; private set; }
         public double IntensidadLluvia { get; private set; }
-        
+        public TipoPeriodo TipoPeriodo
+        {
+            get
+            {
+                if (Sequia) return TipoPeriodo.Sequia;
+                else if (Lluvia) return TipoPeriodo.Lluvia;
+                else if (COPT) return TipoPeriodo.COPT;
+                else return TipoPeriodo.Normal;
+            }
+        }
+
         public Espacio(IList<Planeta> planetas)
         {
             Planetas = planetas;
             DiaActual = 0;
-            //CalcularPerimetroMaximo();
         }
 
         public void CalcularClima()
         {
-            Calcular();
+            CalcularSequia();
+            if (!Sequia)
+            {
+                CalcularLluvia();
+                CalcularPerimetro();
+                if (!Lluvia)
+                {
+                    CalcularCOPT();
+                }
+            }
         }
 
         public void AvanzarDia()
@@ -35,36 +63,10 @@ namespace SistemaSolar
             }
         }
 
-        public string Reportar()
-        {
-            var reporte = new StringBuilder();
-            reporte.AppendLine($"Día: {DiaActual}");
-            if (Sequia)
-            {
-                reporte.AppendLine("Clima: Sequia");
-            }
-            else if (Lluvia)
-            {
-                reporte.Append($"Clima: Lluvia - Intensidad: {IntensidadLluvia}");
-            }
-            else if (CondicionesOptimasPyT)
-            {
-                reporte.AppendLine("Clima: Condiciones óptimas de P y T");
-            }
-            else
-            {
-                reporte.AppendLine("Clima: Normal");
-            }
-
-            foreach (var planeta in Planetas)
-            {
-                reporte.AppendLine($"Planeta {planeta.Nombre.PadRight(12)}--> ({planeta.Angulo:000};{planeta.Distancia:0000}) ({planeta.X:n};{planeta.Y:n})");
-            }
-
-            return reporte.ToString();
-        }
-
-        // Gen
+        /// <summary>
+        /// Calcula el perímetro del triángulo formado por los 3 planetas.
+        /// El resultado se guarda como representación de la intensidad de la lluvia.
+        /// </summary>
         private void CalcularPerimetro()
         {
             var p = 0d;
@@ -77,20 +79,9 @@ namespace SistemaSolar
             IntensidadLluvia = p;
         }
 
-        private void Calcular()
-        {
-            CalcularPerimetro();
-            CalcularSequia();
-            if (!Sequia)
-            {
-                CalcularLluvia();
-                if (!Lluvia)
-                {
-                    CalcularCondicionOptimaPyT();
-                }
-            }
-        }
-
+        /// <summary>
+        /// Determina si el día actual es de tipo Sequia.
+        /// </summary>
         private void CalcularSequia()
         {
             // Los planetas están alineados con el sol cuando entre ellos están todos en el mismo ángulo a a 180°.
@@ -104,21 +95,39 @@ namespace SistemaSolar
                     break;
                 }
             }
+
+            // Si entró acá, no hay lluvia.
+            IntensidadLluvia = 0;
         }
 
+        /// <summary>
+        /// Determina si el día actual es de tipo Lluvia.
+        /// </summary>
         private void CalcularLluvia()
         {
-            // https://en.wikipedia.org/wiki/Barycentric_coordinate_system
-            var lambda1 = ((Planetas[1].Y - Planetas[2].Y) * (0 - Planetas[2].X) + (Planetas[2].X - Planetas[1].X) * (0 - Planetas[2].Y)) / ((Planetas[1].Y - Planetas[2].Y) * (Planetas[0].X - Planetas[2].X) + (Planetas[2].X - Planetas[1].X) * (Planetas[0].Y - Planetas[2].Y));
-            var lambda2 = ((Planetas[2].Y - Planetas[0].Y) * (0 - Planetas[2].X) + (Planetas[0].X - Planetas[2].X) * (0 - Planetas[2].Y)) / ((Planetas[1].Y - Planetas[2].Y) * (Planetas[0].X - Planetas[2].X) + (Planetas[2].X - Planetas[1].X) * (Planetas[0].Y - Planetas[2].Y));
+            // Es tipo lluvia si el sol está dentro del triángulo formado por los 3 planetas.
+            // Se considera al sol como un punto en las coordenadas (0;0).
+            // Para determinar si un punto está dentro de un triángulo, se pasa de coordenadas
+            // cartesianas a baricéntricas.
+            // Transformación de coordenadas:
+            // Coordenadas del sol, el punto que se quiere probar si pertenece al triángulo.
+            var solX = 0;
+            var solY = 0;
+            var lambda1 = ((Planetas[1].Y - Planetas[2].Y) * (solX - Planetas[2].X) + (Planetas[2].X - Planetas[1].X) * (solY - Planetas[2].Y)) / ((Planetas[1].Y - Planetas[2].Y) * (Planetas[0].X - Planetas[2].X) + (Planetas[2].X - Planetas[1].X) * (Planetas[0].Y - Planetas[2].Y));
+            var lambda2 = ((Planetas[2].Y - Planetas[0].Y) * (solX - Planetas[2].X) + (Planetas[0].X - Planetas[2].X) * (solY - Planetas[2].Y)) / ((Planetas[1].Y - Planetas[2].Y) * (Planetas[0].X - Planetas[2].X) + (Planetas[2].X - Planetas[1].X) * (Planetas[0].Y - Planetas[2].Y));
+            // La suma de las coordenadas baricéntricas debe ser 1.
             var lambda3 = 1 - lambda1 - lambda2;
 
-            Lluvia = (0 <= lambda1 && lambda1 <= 1) && (0 <= lambda2 && lambda2 <= 1) && (0 <= lambda3 && lambda3 <= 1);
+            // Condición de pertenencia al triángulo: las 3 coordenadas deben estar entre 0 y 1.
+            Lluvia = lambda1.Between(0, 1) && lambda2.Between(0, 1) && lambda3.Between(0, 1);
         }
 
-        private void CalcularCondicionOptimaPyT()
+        /// <summary>
+        /// Determina si el día actual es de tipo COPT.
+        /// </summary>
+        private void CalcularCOPT()
         {
-            // Ya se verificó que no estaban alineados al sol.
+            // Previamente se tiene que haber verificado que los planetas no estaban alineados al sol.
             var distancias = new double[]
             {
                 CalcularDistancia(0, 1),
@@ -126,16 +135,29 @@ namespace SistemaSolar
                 CalcularDistancia(0, 2)
             };
 
+            // Condición de planetas alineados:
+            // Por una propiedad de los triángulos, la suma de 2 lados siempre es mayor al lado restante.
+            // Cuando hay una igualdad, es que el triángulo no tiene área, es decir es una línea recta.
             var distanciaMayor = distancias.Max();
             var distanciasSuma = distancias.Where(x => x != distanciaMayor).Sum();
             var dif = Math.Abs(distanciaMayor - distanciasSuma);
 
+            // Si entró acá, no hay lluvia.
+            IntensidadLluvia = 0;
+
             var delta = 1;
-            CondicionesOptimasPyT = dif.Between(-delta, delta);
+            COPT = dif.Between(-delta, delta);
         }
 
+        /// <summary>
+        /// Calcula la distancia entre 2 planetas.
+        /// </summary>
+        /// <param name="i">Índice de uno de los planeta.</param>
+        /// <param name="j">Índice del otro planeta.</param>
+        /// <returns>Distancia entre los planetas.</returns>
         private double CalcularDistancia(int i, int j)
         {
+            // Se calcula por Pitágoras, es la hipotenusa del triángulo rectángulo con vértices en ambos planetas.
             var d = Math.Sqrt(Math.Pow(Planetas[i].X - Planetas[j].X, 2) + Math.Pow(Planetas[i].Y - Planetas[j].Y, 2));
             return d;
         }
--- sistemasolar/Planeta.cs
+++ sistemasolar/Planeta.cs
@@ -2,8 +2,12 @@
 
 namespace SistemaSolar
 {
+    /// <summary>
+    /// Representa un planeta con una distancia al sol y una velocidad y sentido de rotación.
+    /// </summary>
     public class Planeta
     {
+        // Se usa para resetear el ángulo.
         private double _anguloInicial;
 
         public double Angulo { get; private set; }
@@ -12,6 +16,14 @@ namespace SistemaSolar
         public int Velocidad { get; private set; }
         public bool SentidoHorario { get; private set; }
 
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="distancia">Distancia al sol en el origen (0;0).</param>
+        /// <param name="velocidad">Velocidad de rotación en grados/día.</param>
+        /// <param name="sentidoHorario">Indica si el planeta gira en sentido horario o anti-horario.</param>
+        /// <param name="nombre">Nombre del planeta.</param>
+        /// <param name="angulo">Ángulo inicial.</param>
         public Planeta(int distancia, int velocidad, bool sentidoHorario, string nombre, double angulo = 0)
         {
             if (distancia <= 0)
@@ -19,6 +31,7 @@ namespace SistemaSolar
                 throw new ArgumentException("La distancia mayor a 0.");
             }
 
+            // El ángulo se normaliza entre 0 y 359.
             while (angulo < 0)
             {
                 angulo += 360;
@@ -38,10 +51,14 @@ namespace SistemaSolar
             _anguloInicial = angulo;
         }
 
+        /// <summary>
+        /// Modifica el ángulo en un día, a partir de la velocidad de giro por día.
+        /// </summary>
         public void AvanzarDia()
         {
             // En sentido horario, el ángulo decrece. 
             Angulo += Velocidad * (SentidoHorario ? -1 : 1);
+            // El ángulo siempre tiene que quedar entre 0 y 359.
             if (Angulo >= 360)
             {
                 Angulo -= 360;
@@ -52,17 +69,26 @@ namespace SistemaSolar
             }
         }
 
+        /// <summary>
+        /// Volver al ángulo inicial.
+        /// </summary>
         public void Reset()
         {
             Angulo = _anguloInicial;
         }
-   
+
+        /// <summary>
+        /// Coordenada cartesiana X.
+        /// </summary>
         public double X
         {
             // La distancia al sol es la hipotenusa. Lado adyacente = X = coseno(angulo) * hipotenusa.
             get { return Math.Cos(Angulo * Math.PI / 180d) * Distancia; }
         }
 
+        /// <summary>
+        /// Coordenada cartesiana Y.
+        /// </summary>
         public double Y
         {
             // La distancia al sol es la hipotenusa. Lado opuesto = Y = seno(angulo) * hipotenusa.
